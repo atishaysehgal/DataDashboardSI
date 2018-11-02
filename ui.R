8920a870c9cd2009cdadd552a1ab66bac62bea8c
Atishay Sehgal <as5453@columbia.edu>
2018-11-02 01:08:34 -0400

    ui and server
    ui and server

--- ui.R
+++ ui.R
@@ -2,33 +2,24 @@ library(shiny)
 library(dplyr)
 
 
-ui <- fluidPage(
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(inputId = "Demog",label = "Factor:",choices = c("HH Income" = "Income",
-                                                                  "Age Group" = "Age",
-                                                                  "US Region" = "Region") , selected = "Age"),
-      selectInput(inputId = "Car",label = "VW Model:",choices = c("BEETLE" = "BEETLE",
-                                                                  "CC" = "CC",
-                                                                  "EOS" = "EOS",
-                                                                  "GOLF" = "GOLF",
-                                                                  "GTI" ="GOLF SPORTSWAGEN GTI",
-                                                                  "JETTA" = "JETTA",
-                                                                  "PASSAT" = "PASSAT",
-                                                                  "TIGUAN" = "TIGUAN",
-                                                                  "TOUAREG" = "TOUAREG") , selected = "BEETLE"),
-      radioButtons(inputId = "Metric",label ="Measurement Type",choices = 
-                     c("Conquest Volume Index" = "TotCmpConqVol_IDX","C/D Ratio" = "TotCmpCDRatio_IDX"), selected = "TotCmpConqVol_IDX" )                
-      
-    )
-  ),
-  mainPanel(
-    tags$h1("The Bar Charts"),
-    tags$h2("The metrics"),
-    
-    
-    plotOutput("P1")
-    
-  )
+# Define UI for miles per gallon application
+shinyUI(pageWithSidebar(
   
-)
+  # Application title
+  headerPanel("Healthcare"),
+  
+  # Sidebar with controls to select the variable to plot against mpg
+  # and to specify whether outliers should be included
+  sidebarPanel(
+    selectInput("variable", "Factor: ",
+                list("Uninsured %" = "uninsured", 
+                     "Primary Care Physicians" = "primary_care_physicians", 
+                     "Mental Health Providers" = "mental_health_providers",
+                     "Dentists" = "dentists"
+                     ))
+  ),
+  
+  mainPanel(
+    h3(textOutput("caption")),
+    plotOutput("yearPlot"))
+))
